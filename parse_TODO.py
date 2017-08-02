19afa72f35f5cdd3937b60bb28d8bc4d0c679261
marcosbrito <marbri91@gmail.com>
2017-08-02 18:47:44 +0200

    Create script to filter file & store the results
    The results will be stored in an string list.

--- parse_TODO.py
+++ parse_TODO.py
@@ -1,7 +1,24 @@
 import sys
 
+# Filter the file and stores the TODO comments separated in
+# the todo_list string list.
 def main(argv):
-    print("Hola");
+
+    todo_list = list();
+    strg = "";
+    with open(argv[1]) as inf:
+        for line in inf:
+            if line.startswith("// TODO"):
+                # Remove the '// TODO ' part of the line
+                strg = line.split("// TODO ",1)[1];
+            elif strg != "" and line.startswith("//"):
+                    strg = strg + line.split("//",1)[1];
+            elif strg != "":
+                # Filter the string so it will remove blank lines
+                todo_list.append("\n".join([i for i in strg.split("\n") if i.split()]));
+                strg = "";
+    for l in todo_list:
+        print(l);
 
 if __name__ == "__main__":
     main(sys.argv)

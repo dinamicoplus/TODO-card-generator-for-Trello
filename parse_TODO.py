fe889913dee9be21b73959855066398290d74428
marcosbrito <marbri91@gmail.com>
2017-08-03 17:11:56 +0200

    Add class Trello_card to parse_TODO.py
    This will give an structure to the information in the TODO comment and will
    allow more features in the future

--- parse_TODO.py
+++ parse_TODO.py
@@ -7,9 +7,15 @@ TODO card generator for Trello
 License: MIT License
 '''
 
+import datetime
 import sys
 import requests
 import json
+
+class Trello_card:
+    def __init__(self,name = "",desc = ""):
+        self.name = name;
+        self.desc = desc;
 
 # Calls Trello API to get the TODO list id
 # inputs:   board = String, key = String, token = String
@@ -49,18 +55,25 @@ def clean_string(string):
 # output:   String list
 def filter_file(o_file):
     todo_list = list();
-    strg = "";
+    desc = "";
+    card = None;
     with open(o_file) as inf:
         for line in inf:
             if line.startswith("// TODO"):
+                card = Trello_card();
                 # Remove the '// TODO ' part of the line
-                strg = line.split("// TODO ",1)[1];
-            elif strg != "" and line.startswith("//"):
-                    strg = strg + line.split("//",1)[1];
-            elif strg != "":
+                card.name = line.split("// TODO ",1)[1].strip();
+                if card.name == "":
+                    card.name = "TODO - " + datetime.datetime.now().strftime("%Y-%m-%d");
+
+            elif card != None and line.startswith("//"):
+                    desc = desc + line.split("//",1)[1];
+            elif card != None:
                 # Filter the string so it will remove blank lines
-                todo_list.append(clean_string(strg));
-                strg = "";
+                card.desc = clean_string(desc);
+                todo_list.append(card);
+                desc = "";
+                card = None;
     return todo_list;
 
 def main(argv):
@@ -69,11 +82,11 @@ def main(argv):
     key = argv[3];
     token = argv[4];
 
-    todo_text = filter_file(file_path);
+    todo_cards = filter_file(file_path);
     todo_list_id = list_id_API_request(board_id,key,token);
 
-    for idx, l in enumerate(todo_text):
-        post_card_API_request(todo_list_id, key, token, "TODO n" + str(idx), desc = l);
+    for idx, card in enumerate(todo_cards):
+        post_card_API_request(todo_list_id, key, token, card.name, desc = card.desc);
 
 if __name__ == "__main__":
     main(sys.argv)
